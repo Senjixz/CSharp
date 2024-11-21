cdc570da36e7f669d476ed78f4e853060336adad
Senjixz <stiandlay915@gmail.com>
2024-11-21 12:33:30 +0200

    Add existing file

---
+++ LoopsInC#/LoopsInC#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to repeat? ");
+            string message = Console.ReadLine();
+
+            Console.Write("How many times do you want to repeat the number?");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+
+            if (loopCounter <=0)
+            {
+                Console.WriteLine("Apologies, please enter enter a value above 0");
+            }
+            else
+            {
+
+
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+                Console.WriteLine("Loop has finished");
+            }
+        }
+    }
+}
