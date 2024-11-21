0de32679746eade1a0a37eee3cd69660a570d5e1
Senjixz <stiandlay915@gmail.com>
2024-11-21 13:24:12 +0200

    Add existing file

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 5)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.WriteLine("The loop has finished");
+        }
+    }
+}
