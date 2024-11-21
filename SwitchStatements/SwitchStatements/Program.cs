ef3ffa4e4af37d8160f07466be22ea08df97d264
Senjixz <stiandlay915@gmail.com>
2024-11-21 12:07:30 +0200

    Add existing file

---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            switch(grade)
+            {
+                case 5:
+                    Console.WriteLine("Excellent!");
+                    break;
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 2:
+                    Console.WriteLine("Ok");
+                    break;
+                case 1:
+                    Console.WriteLine("Needs Improvement");
+                    break;
+                case 0:
+                    Console.WriteLine("Fail");
+                    break;
+            }
+        }
+    }
+}
