a4419f177d0f008006a379ab1bc065e602829871
Senjixz <stiandlay915@gmail.com>
2024-11-26 10:53:33 +0200

    Added file

---
+++ OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even, " + "(" + number + ")");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd, " + "(" + number + ")");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
