a93160698c7c1f579414b9d5c430b4640b0c159a
Senjixz <stiandlay915@gmail.com>
2024-11-26 13:26:44 +0200

    Added file

---
+++ ArrrayMultiplication/ArrrayMultiplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrrayMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int length = 5;
+
+            int[] multiplicationArray = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                multiplicationArray[i] = num1 * (i + 1);
+            }
+
+            Console.WriteLine($"Multiplication Table for {num1}:");
+            foreach (int value in multiplicationArray)
+            {
+                Console.WriteLine(value);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
