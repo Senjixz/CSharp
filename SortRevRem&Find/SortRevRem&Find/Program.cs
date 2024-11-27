75c11a945d19bd8ed6044a925a0f7e944c5ed915
Senjixz <stiandlay915@gmail.com>
2024-11-27 12:48:58 +0200

    Added file

---
+++ SortRevRem&Find/SortRevRem&Find/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortRevRem_Find
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                45, 12, 78, 34, 89, 23,
+            };
+
+            foreach(var orig in numbers)
+            {
+                Console.WriteLine(orig);
+            }
+
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+
+            int numToRemove = 78;
+            numbers = numbers.Where(val => val != numToRemove).ToArray();
+            foreach (int num in numbers)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+            int search = 34;
+
+            int position = Array.IndexOf(numbers, search);
+
+            if (position > -1)
+            {
+                Console.WriteLine($"{search} has been found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} does not exist");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
