ba674a238dc590e00c301cb1853f42bcadd98a99
Senjixz <stiandlay915@gmail.com>
2024-12-03 11:21:41 +0200

    Added file

---
+++ LogicalError2/LogicalError2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalError2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an age number");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 29)
+            {
+                Console.WriteLine("Over 18");
+            }
+            else if (age < 39)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age < 100)
+            {
+                Console.WriteLine("Over 50");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
