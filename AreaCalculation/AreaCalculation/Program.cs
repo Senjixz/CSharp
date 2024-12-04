40e2c445e0ca5ed999106a33b66057217150f333
Senjixz <stiandlay915@gmail.com>
2024-12-04 10:49:29 +0200

    Added file

---
+++ AreaCalculation/AreaCalculation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculation
+{
+    class Triangle
+    {
+        public static double CalculateArea(Double width, Double height)
+        {
+            return (width * height) / 2;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of the triangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the height of the triangle: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double area = Triangle.CalculateArea(width, height);
+            Console.WriteLine($"The area of the triangle is {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
