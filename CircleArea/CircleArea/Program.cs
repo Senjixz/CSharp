066b5f3308d97d77d2439ca0252dce950c16c7eb
Senjixz <stiandlay915@gmail.com>
2024-12-05 11:23:02 +0200

    Added File

---
+++ CircleArea/CircleArea/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class circle
+    {
+        public double Radius { get; set; }
+
+        public circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circle circle = new circle(1.5);
+            double area = circle.CalculateArea();
+            Console.WriteLine($"Area of the circle is: {area}");
+            Console.ReadLine();
+        }
+    }
+}
