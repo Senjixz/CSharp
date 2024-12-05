2be19b7143d17b54182a9ce7c595ceef032ebefe
Senjixz <stiandalay915@gmail.com>
2024-12-05 14:15:08 +0200

    Add files via upload

---
+++ TryParseTask1/TryParseTask1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a text: ");
+
+                int num = something(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine($"Conversion succeeded: {Console.ReadLine()}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            Console.ReadLine();
+        }
+        static int something(int yuh)
+        {
+            Convert.ToString(yuh);
+            if (yuh == 0)
+            {
+                Console.WriteLine($"Conversion succeeded: {Console.ReadLine()}");
+            }
+            else if (yuh == 1)
+            {
+                Console.WriteLine($"Conversion failed: {Console.ReadLine()}");
+            }
+            return yuh;
+        }
+    }
+    }
+}
