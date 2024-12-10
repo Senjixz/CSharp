c213de2c039d1eb339cd53e81feb24a8ffddb974
Senjixz <stiandalay915@gmail.com>
2024-12-10 12:33:45 +0200

    Made changes to the file

--- DictionaryCaseTask/DictionaryCaseTask/Program.cs
+++ DictionaryCaseTask/DictionaryCaseTask/Program.cs
@@ -19,56 +19,60 @@ namespace DictionaryCaseTask
                 {3, "Show all" },
                 {4, "Exit" }
             };
-          
-            for (int i = 0; i < options.Count; i++)
-            {
-                KeyValuePair<int, string> pair = options.ElementAt(i);
-                Console.WriteLine($"{pair.Key} - {pair.Value}");
-            }
 
-            List<string> list = new List<string>()
+            bool exit = false;
+            while (!exit)
+            {
+
+                for (int i = 0; i < options.Count; i++)
+                {
+                    KeyValuePair<int, string> pair = options.ElementAt(i);
+                    Console.WriteLine($"{pair.Key} - {pair.Value}");
+                }
+
+                List<string> list = new List<string>()
             {
                 "Math Bob"
             };
 
 
-            Console.WriteLine("Choose option between 1 and 4");
-            int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose option between 1 and 4");
+                int option = Convert.ToInt32(Console.ReadLine());
 
-            if (option == 1)
-            {
-                Console.WriteLine("Enter the name of the new subject: ");
-                string newSubject = Console.ReadLine();
-                Console.WriteLine("Teachers name: ");
-                string newTeacher = Console.ReadLine();
+                if (option == 1)
+                {
+                    Console.WriteLine("Enter the name of the new subject: ");
+                    string newSubject = Console.ReadLine();
+                    Console.WriteLine("Teachers name: ");
+                    string newTeacher = Console.ReadLine();
 
-                string newList = newSubject + newTeacher;
+                    string newList = newSubject + newTeacher;
 
-                list.Add(newList);
-                Console.WriteLine("Subject and teacher added");
+                    list.Add(newList);
+                    Console.WriteLine("Subject and teacher added");
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("Enter the name of the subject you wish to remove: ");
+                    list.Remove(Console.ReadLine());
+                    Console.WriteLine("Subject successfully removed");
+                }
+
+                switch (option)
+                {
+                    case 3:
+                        Console.WriteLine("Showing all");
+                        Console.WriteLine(list);
+                        break;
+                    case 4:
+                        exit = true;
+                        Console.WriteLine("Exiting program");
+                        break;
+                }
+                Console.ReadLine();
             }
-            else if (option == 2)
-            {
-                Console.WriteLine("Enter the name of the subject you wish to remove: ");
-                list.Remove(Console.ReadLine());
-                Console.WriteLine("Subject successfully removed");
-            }
-
-            switch (option)
-            {
-                case 3:
-                    Console.WriteLine("Showing all");
-                    Console.WriteLine(list);
-                    break;
-                case 4:
-                    Console.WriteLine("Exiting program");
-                    break;
-            }
-
-
-
-
-            Console.ReadLine();
+            
+            
         }
     }
 }
